49dc7a62bd62bf8fba2587fbfbf561543c7c87bc
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-09-01 12:11:07 +0800

    Chore: add TextView and MaterialButton objects to main.java

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mycalculator;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
+    TextView solutionTv, resultTv;
+    MaterialButton btnC, btnBrackOpen, btnBrackClose,
+                   btnDiv, btnMult, btnPlus, btnMinus, btnEquals,
+                   btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
